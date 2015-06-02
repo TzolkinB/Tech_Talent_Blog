7e209e35edf8a8ad46c0bc9c0e18fff8fd21e13c
TzolkinB <krbell4@gmail.com>
2015-06-02 14:50:14 +0000

    added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
 
   resources :blog_posts
+	
+	root 'blog_posts#index' # controller hash action/method, blog_post controller# index method
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
